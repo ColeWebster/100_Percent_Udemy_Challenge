eed27b2e8ee650dc257e0c567d99f55cb7c1001e
Cole Webster <CWebster_Coding@outlook.com>
2023-07-29 17:23:47 -0400

    Adding the challenge folder for 7

--- Modern_JavaScript_BootCamp/Section_7/notes-app.js
+++ Modern_JavaScript_BootCamp/Section_7/notes-app.js
@@ -1,2 +1,22 @@
-console.log("testing the link to external JS")
-console.log("Updated")
+// console.log("testing the link to external JS")
+// console.log("Updated")
+
+// DOM = Document Object Model
+
+// Query and remove
+// const p = document.querySelector('p');
+// p.remove();
+
+// Query all and remove
+const ps = document.querySelectorAll('p');
+ps.forEach(function (p){
+    // p.remove();
+    // console.log(p.textContent);
+    // p.textContent = '******';
+})
+
+/* 
+p.remove removes all p tags from the query selector
+p.textContent = console logging all p tags
+p.textContent = changing all p tags to the string include
+*/ 
