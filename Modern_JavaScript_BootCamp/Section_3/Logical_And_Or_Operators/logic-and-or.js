f66c6bed25363ba143bf010c4f1b71ed6ceb0a6e
Cole Webster <CWebster_Coding@outlook.com>
2023-08-20 20:26:37 -0400

    Added or operatrot

--- Modern_JavaScript_BootCamp/Section_3/Logical_And_Or_Operators/logic-and-or.js
+++ Modern_JavaScript_BootCamp/Section_3/Logical_And_Or_Operators/logic-and-or.js
@@ -1,6 +1,13 @@
 let temp = 65;
 
 // Logical AND operator
+// True is both sides are true, False otherwise
 if (temp >= 60 && temp <= 90) {
     console.log("It is really nice out!")
+}
+
+// Logical OR operator
+// True if at least one side is true. False otherwise
+if (temp <= 0 || temp >= 115) {
+    console.log("It is dangerous to go outside")
 }
